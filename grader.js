0848b90bdbda8d6054eb8244aaccfcbf5cd04bcc
Maxburn <maxburn@gmail.com>
2013-07-12 01:42:52 -0500

    updated grader to include URL. updated checks to include more checks

--- grader.js
+++ grader.js
@@ -24,8 +24,11 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URLFILE_DEFAULT = "urlFile.txt";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -61,12 +64,33 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var buildfn = function() {
+    var response2console = function(result, response) {
+        if (result instanceof Error) {
+            console.error('Error: ' + util.format(response.message));
+        } else {
+            fs.writeFileSync(URLFILE_DEFAULT, result);
+            return result;
+        }
+    };
+    return response2console;
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to check')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    var checkJson;
+    if (program.url) {
+        var response2console = buildfn();
+        var resultOfURL = rest.get(program.url).on('complete', response2console);
+        checkJson = checkHtmlFile(URLFILE_DEFAULT, program.checks);
+    }
+    else {
+     checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
